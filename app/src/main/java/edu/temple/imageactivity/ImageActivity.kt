138e74cb006c04ba96bfc171877f8e734b797e75
stephull <shullender0907@gmail.com>
2021-09-23 20:38:44 -0400

    move data class; add image text, potential recyclerview xml, and grid layout man.

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -2,13 +2,12 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.*
 import android.widget.*
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ImageActivity : AppCompatActivity() {
 
-    data class ImageObject (val desc: String, val imageSrc: Int)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,20 +18,28 @@ class ImageActivity : AppCompatActivity() {
         val imageMenu : RecyclerView = findViewById(R.id.imageMenu)
 
         // declare all image sources and text:
-        val allImages : Array<ImageObject> = arrayOf(
-            ImageObject("", R.drawable.astronaut_in_space),
-            ImageObject("", R.drawable.aurora_borealis),
-            ImageObject("", R.drawable.blue_clouds),
-            ImageObject("", R.drawable.desert),
-            ImageObject("", R.drawable.diving_in_the_ocean),
-            ImageObject("", R.drawable.grasslands),
-            ImageObject("", R.drawable.high_waterfall),
-            ImageObject("", R.drawable.lush_forest),
-            ImageObject("", R.drawable.moon),
-            ImageObject("", R.drawable.rainbow),
-            ImageObject("", R.drawable.rice_fields),
-            ImageObject("", R.drawable.sunset_and_tree)
+        val allImages : Array<MyPicture> = arrayOf(
+            MyPicture("Astronaut in Space", R.drawable.astronaut_in_space),
+            MyPicture("Aurora Borealis", R.drawable.aurora_borealis),
+            MyPicture("Blue Clouds", R.drawable.blue_clouds),
+            MyPicture("Desert", R.drawable.desert),
+            MyPicture("Diving in the Ocean", R.drawable.diving_in_the_ocean),
+            MyPicture("Grasslands", R.drawable.grasslands),
+            MyPicture("High Waterfall", R.drawable.high_waterfall),
+            MyPicture("Lush Forest", R.drawable.lush_forest),
+            MyPicture("Moon", R.drawable.moon),
+            MyPicture("Rainbow", R.drawable.rainbow),
+            MyPicture("Rice Fields", R.drawable.rice_fields),
+            MyPicture("Sunset and Tree", R.drawable.sunset_and_tree)
         )
+
+        // properties of the recycler view + grid layout manager
+        val glm = GridLayoutManager(this, 2)
+        imageMenu.layoutManager = glm
+
+        // assemble adapter + new layout file (inside adapter file)
+
+        // in the event that the user selects the image from the menu
 
     }
 }
---
+++ app/src/main/java/edu/temple/imageactivity/MyPicture.kt
@@ -0,0 +1,3 @@
+package edu.temple.imageactivity
+
+data class MyPicture(val desc: String, val imageSrc: Int)
